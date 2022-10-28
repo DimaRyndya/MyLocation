04d4204ec2d95984c4dfa700405cd798de0a42de
DimaRyndya <dmitriy.ryndya@gmail.com>
2022-10-28 20:04:16 +0300

    Initial Commit

---
+++ MyLocation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocation
+//
+//  Created by Dmitriy Ryndya on 28.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
