450f02b7481686bd90e6abce39fe3e1036dfdedc
DimaRyndya <dmitriy.ryndya@gmail.com>
2022-11-03 15:05:56 +0200

    create data model with Core Data implement saving and fatching data

---
+++ MyLocation/LocationsViewController.swift
@@ -0,0 +1,51 @@
+import UIKit
+import CoreData
+import CoreLocation
+
+class LocationsViewController: UITableViewController {
+    var managedObjectContext: NSManagedObjectContext!
+
+    var locations: [Location] = []
+
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      // 1
+      let fetchRequest = NSFetchRequest<Location>()
+      // 2
+      let entity = Location.entity()
+      fetchRequest.entity = entity
+      // 3
+      let sortDescriptor = NSSortDescriptor(
+        key: "date",
+        ascending: true)
+      fetchRequest.sortDescriptors = [sortDescriptor]
+      do {
+        // 4
+        locations = try managedObjectContext.fetch(fetchRequest)
+      } catch {
+        fatalCoreDataError(error)
+      }
+    }
+
+
+    // MARK: - Table View Delegates
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return locations.count
+    }
+
+    override func tableView(
+      _ tableView: UITableView,
+      cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(
+        withIdentifier: "LocationCell",
+        for: indexPath) as! LocationCell
+
+      let location = locations[indexPath.row]
+      cell.configure(for: location)
+
+      return cell
+    }
+
+}
+
