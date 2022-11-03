6eb64bed2a052781bf6657997d4a64c33c450916
DimaRyndya <dmitriy.ryndya@gmail.com>
2022-11-03 21:22:09 +0200

    add mapView screen and MapView on It

---
+++ MyLocation/MapViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+import MapKit
+import CoreData
+
+class MapViewController: UIViewController {
+    @IBOutlet var mapView: MKMapView!
+
+    var managedObjectContext: NSManagedObjectContext!
+
+    // MARK: - Actions
+    @IBAction func showUser() {
+        let region = MKCoordinateRegion(
+            center: mapView.userLocation.coordinate,
+            latitudinalMeters: 1000,
+            longitudinalMeters: 1000)
+        mapView.setRegion(
+            mapView.regionThatFits(region),
+            animated: true)
+    }
+
+    @IBAction func showLocations() {
+    }
+}
+
+extension MapViewController: MKMapViewDelegate {
+}
